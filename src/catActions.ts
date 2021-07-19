c7263a542d2faa7dc95b66d2dae2d4784f51f8f9
laurence fass <laurencefass@hotmail.com>
2021-07-19 13:51:17 +0100

    moved API calls from components into Redux thunk wrappers

--- src/catActions.ts
+++ src/catActions.ts
@@ -1,3 +1,16 @@
+import {trace, traceCurrent} from './utils'
+
+import {
+  apiGetVoteById,
+  apiSubmitVote,
+  apiAddFavourite,
+  apiDeleteFavourite,
+  apiGetFavouriteById,
+  apiGetMyCats,
+  apiGetFavourites,
+  apiGetVotes,
+} from "./CatAPI";
+
 import { 
   ECatActionTypes, 
   ICatAction, 
@@ -8,6 +21,8 @@ import {
   TVote, 
   TFavourite 
 } from './types'
+
+
 
 export function CollectionAction(collection: Array<TCat>) : ICatAction {
   return {
@@ -64,3 +79,96 @@ export function VoteDownAction(vote: TVote) : ICatAction {
     payload: vote
   };
 }
+
+export const initCatsThunk = () => {
+  traceCurrent("initCatsThunk");
+  return dispatch => {
+    traceCurrent("initCatsThunk.dispatch");
+    try {
+      (async () => {
+        trace("CatUI.getMyCats")
+        let cats = await apiGetMyCats();
+        trace("CatUI.getMyCats success", cats);
+        dispatch(CollectionAction(cats));
+  
+        let favourites = await apiGetFavourites();
+        trace("CATUI.apiGetFavourites", favourites);
+        dispatch(FavouritesAction(favourites));
+  
+        let votes = await apiGetVotes();
+        trace("CATUI.apiGetFavourites", votes);
+        dispatch(VotesAction(votes));  
+      })();    
+    }
+    catch(e) {
+      throw new Error("initcatsThunk" + e.Message);;
+    }
+  }
+}
+
+export const unFavouriteThunk = (id: string) => {
+  return dispatch => {
+    try {
+      (async (id) => {
+        let favourite = await apiDeleteFavourite(id);
+        if (!favourite)
+          throw new Error("setFavouriteReducer favourite object not returned from server"); 
+        dispatch(UnFavouriteAction(id))
+      })(id);  
+    }
+    catch(e) {
+      alert("CATUI connection error" + e.message);
+    }  
+  }
+}
+
+
+export const addFavouriteThunk = (id:string) => {
+  return dispatch => {
+    try {
+      (async (id) => {
+        let favourite = await apiAddFavourite(id);
+        if (!favourite)
+          throw new Error("setFavouriteReducer favourite object not returned from server"); 
+        favourite = await apiGetFavouriteById(favourite.id);
+        if (!favourite)
+          throw new Error("setFavouriteReducer favourite object not returned from server");
+        dispatch(SetFavouriteAction(favourite));
+      })(id);    
+    }
+    catch(e) {
+      alert("CATUI connection error" + e.message);
+    }
+  }
+}
+
+// thunk actions
+export const handleVoteThunk = (id:string, value:number) => {
+  traceCurrent("handleVoteThunk");
+
+  return dispatch => {
+      traceCurrent("handleVoteThunk.dispatch");
+      try {
+      (async () => {
+        let vote = await apiSubmitVote(id, value ? true : false);
+        if (!vote)
+          throw new Error("VoteControl favourite object not returned from server"); 
+
+        vote = await apiGetVoteById(vote.id);
+        if (!vote)
+          throw new Error("VoteControl favourite object not returned from server");
+        
+        // dispatch(VotesAction([...votes.concat(vote)]));
+        if (value) {
+          dispatch(VoteUpAction(vote));
+        } else {
+          dispatch(VoteDownAction(vote));
+        } 
+      })();    
+    }
+    catch(e) {
+      alert("CATUI connection error" + e.message);
+    }
+  }
+}
+
